34931010c2aabd940bdb2c3ac4585eb6656f885e
Sharlotte <60801210+Sharlottes@users.noreply.github.com>
2021-02-02 23:37:35 +0900

    가독성 업
    테스트 안함 ㅎ

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,20 @@
 if(!Vars.headless){
 
-function tp(){
-Vars.player.unit().set(Core.input.mouseWorld());
-Fx.teleportOut.at(Vars.player.x, Vars.player.y, Pal.lancerLaser);
-Vars.player.snapInterpolation();
-}
+    function tp(){
+        Vars.player.unit().set(Core.input.mouseWorld());
+        Fx.teleportOut.at(Vars.player.x, Vars.player.y, Pal.lancerLaser);
+        Vars.player.snapInterpolation();
+    }
 
-var down = false;
+    var down = false;
 
-Events.run(Trigger.update, () => {
-if(Core.input.ctrl() && Core.input.alt() && Core.input.isTouched() && !Vars.state.isMenu()){
-if(!down){
-down = true;
-tp();
-}
-}
-else{
-down = false;
-}
-});
-
+    Events.run(Trigger.update, () => {
+        if(Core.input.ctrl() && Core.input.alt() && Core.input.isTouched() && !Vars.state.isMenu()){
+            if(!down){
+                down = true;
+                tp();
+            }
+        }
+        else down = false;
+    });
 }
